725979e0566684ea493eda400d9b506857f676e4
dulainsj7-wq <dulainsj7@gmail.com>
2026-05-19 16:47:58 +0530

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'zephyros-v1';
+const ASSETS = [
+  './zephyros_lavender_fixed.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
